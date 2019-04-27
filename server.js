23c201f3461aa5275cbf3b30bd930ebf53fce861
violettebec <bechoerner@gmail.com>
2019-04-27 13:58:20 -0500

    addign passport js and congfig folder

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const passport = require("./config/passport");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//passport middleware
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
