84088c089c22762e78d008167fe23e373932c258
Eric Northrop <enorthrop@google.com>
2019-05-07 15:31:53 -0500

    readability and filepath changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 // import React from 'react';
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar";
+// Import Components
+import Navbar from "./components/NavBar";
 import Home from './components/Home';
 import SigninForm from './components/SigninForm';
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/landingPage";
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Navbar/>
-      <BrowserRouter>
-        <Switch>
-        <Route exact path="/" component={LandingPage} />
-          <Route path="/SigninForm" component={SigninForm} />
-          <Route path="/Home" component={Home} />
-        </Switch>
-      </BrowserRouter>
+
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/signinform" component={SigninForm} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
